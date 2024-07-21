4b6c1113af5b7ff85b9026d29fdd68820751a47c
basnetnarayan <basnetnarayan57972@gmail.com>
2024-07-21 18:26:24 +0545

    hello

--- Lab7/student_result.php
+++ Lab7/student_result.php
@@ -4,14 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Student Scores</title>
     <style>
-        th, td, table
-        {
+        th, td, table {
             border: 2px solid black;
             border-collapse: collapse;
         }
-        tr:nth-child(2n+1){
+        tr:nth-child(2n+1) {
             background-color: antiquewhite;
         }
     </style>
@@ -19,70 +18,80 @@
 
 <body>
     <?php
-    $student = [
+    $students = [
         "Narayan" => [
+            "maths" => ["practical" => 25, "theory" => 50],
             "physics" => ["practical" => 20, "theory" => 30],
-            "biology" => ["practical" => 10, "theory" => 40],
-            "chemistry" => ["practical" => 50, "theory" => 30],
+            "chemistry" => ["practical" => 35, "theory" => 45],
+            "biology" => ["practical" => 18, "theory" => 42],
+            "english" => ["practical" => 0, "theory" => 60],
         ],
         "Mohan" => [
-            "physics" => ["practical" => 20, "theory" => 30],
-            "biology" => ["practical" => 10, "theory" => 40],
-            "chemistry" => ["practical" => 50, "theory" => 30],
+            "maths" => ["practical" => 20, "theory" => 55],
+            "physics" => ["practical" => 22, "theory" => 33],
+            "chemistry" => ["practical" => 40, "theory" => 35],
+            "biology" => ["practical" => 25, "theory" => 40],
+            "english" => ["practical" => 0, "theory" => 65],
         ],
         "Rama" => [
-            "physics" => ["practical" => 20, "theory" => 30],
-            "biology" => ["practical" => 10, "theory" => 40],
-            "chemistry" => ["practical" => 50, "theory" => 30],
+            "maths" => ["practical" => 30, "theory" => 45],
+            "physics" => ["practical" => 28, "theory" => 32],
+            "chemistry" => ["practical" => 38, "theory" => 42],
+            "biology" => ["practical" => 20, "theory" => 35],
+            "english" => ["practical" => 0, "theory" => 70],
         ],
         "Bibek" => [
-            "physics" => ["practical" => 20, "theory" => 30],
-            "biology" => ["practical" => 10, "theory" => 40],
-            "chemistry" => ["practical" => 50, "theory" => 30],
+            "maths" => ["practical" => 27, "theory" => 48],
+            "physics" => ["practical" => 25, "theory" => 35],
+            "chemistry" => ["practical" => 32, "theory" => 38],
+            "biology" => ["practical" => 30, "theory" => 40],
+            "english" => ["practical" => 0, "theory" => 55],
         ],
         "Smile" => [
-            "physics" => ["practical" => 20, "theory" => 30],
-            "biology" => ["practical" => 10, "theory" => 40],
-            "chemistry" => ["practical" => 50, "theory" => 30],
+            "maths" => ["practical" => 22, "theory" => 50],
+            "physics" => ["practical" => 21, "theory" => 31],
+            "chemistry" => ["practical" => 35, "theory" => 40],
+            "biology" => ["practical" => 23, "theory" => 37],
+            "english" => ["practical" => 0, "theory" => 68],
         ]
-
     ];
     ?>
-        <table>
+    <table>
+        <tr>
+            <th rowspan="2">Name</th>
+            <th colspan="3">Maths</th>
+            <th colspan="3">Physics</th>
+            <th colspan="3">Chemistry</th>
+            <th colspan="3">Biology</th>
+            <th colspan="3">English</th>
+        </tr>
+        <tr>
+            <th>Theory</th>
+            <th>Practical</th>
+            <th>Total</th>
+            <th>Theory</th>
+            <th>Practical</th>
+            <th>Total</th>
+            <th>Theory</th>
+            <th>Practical</th>
+            <th>Total</th>
+            <th>Theory</th>
+            <th>Practical</th>
+            <th>Total</th>
+            <th>Theory</th>
+            <th>Practical</th>
+            <th>Total</th>
+        </tr>
+        <?php foreach ($students as $name => $subjects) { ?>
             <tr>
-                <th  rowspan="2">Name</th>
-                <th colspan="3">Physics</th>
-                <th colspan="3">Biology</th>
-                <th colspan="3">Chemistry</th>
+                <td><?= htmlspecialchars($name) ?></td>
+                <?php foreach ($subjects as $subject) { ?>
+                    <td><?= htmlspecialchars($subject["theory"]) ?></td>
+                    <td><?= htmlspecialchars($subject["practical"]) ?></td>
+                    <td><?= htmlspecialchars($subject["theory"] + $subject["practical"]) ?></td>
+                <?php } ?>
             </tr>
-            <tr>
-                <th>Theory</th>
-                <th>Practical</th>
-                <th>Total</th>
-                <th>Theory</th>
-                <th>Practical</th>
-                <th>Total</th>
-                <th>Theory</th>
-                <th>Practical</th>
-                <th>Total</th>
-            </tr>
-    <?php foreach ($student as $key => $value) {
-    ?>
-    
-            <tr>
-                <td><?=$key?></td>
-
-                <?php foreach ($value as $key1 => $value1) {
-                   ?>
-                   <td><?=$value1["theory"]?></td>
-                   <td><?=$value1["practical"]?></td>
-                   <td><?=$value1["practical"]+$value1["theory"]?></td>
-                   <?php
-                } ?>
-            </tr>
-            <?php
-    }
-    ?>
+        <?php } ?>
     </table>
 </body>
 
